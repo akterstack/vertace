b13f07fe3f24e1e53fe4740347e2cf93a0a20a15
jRoadie <akter.hossain.ict@gmail.com>
2016-10-29 19:11:08 +0600

    updating lifecycle

--- src/main/java/io/vertace/Vertace.java
+++ src/main/java/io/vertace/Vertace.java
@@ -3,35 +3,59 @@ package io.vertace;
 import io.vertace.core.VertaceVerticle;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public abstract class Vertace {
 
     private String[] args;
     private Class<? extends Vertace> vertaceClass;
+    private List<Class<? extends VertaceVerticle>> listOfRegisteredVerticles;
 
     public static void run(Class<? extends Vertace> vertaceAppClass, String[] args) {
-        Vertace vertace = null;
+        Vertace appVertace = null;
         try {
-            vertace = vertaceAppClass.newInstance();
+            appVertace = vertaceAppClass.newInstance();
         } catch(InstantiationException | IllegalAccessException e) {
             e.printStackTrace();
         }
-        vertace.initialize(vertaceAppClass, args);
-        vertace._registerVerticles();
+        appVertace.initialize(vertaceAppClass, args);
+        List<Class<? extends VertaceVerticle>> loadedVerticles = appVertace.loadVerticles();
+        appVertace.registerVerticles(loadedVerticles);
     }
 
     private void initialize(Class<? extends Vertace> vertaceClass, String[] args) {
         this.args = args;
         this.vertaceClass = vertaceClass;
+        this.listOfRegisteredVerticles = new LinkedList<>();
     }
 
-    private List<Class<? extends VertaceVerticle>> _registerVerticles() {
-        return registerVerticles();
+    private List<Class<? extends VertaceVerticle>> loadVerticles() {
+        List<Class<? extends VertaceVerticle>> loadedVerticles = new ArrayList<>();
+        //TODO: load from appconf and call onLoadVerticles(loadedVerticles)
+        onLoadVerticles(loadedVerticles);
+        return loadedVerticles;
     }
 
-    public List<Class<? extends VertaceVerticle>> registerVerticles() {
-        return new ArrayList<>();
+    /**
+     * Lifecycle method
+     * @param loadedVerticles
+     */
+    public void onLoadVerticles(List<Class<? extends VertaceVerticle>> loadedVerticles) {
+
+    }
+
+    private void registerVerticles(List<Class<? extends VertaceVerticle>> loadedVerticles) {
+        onRegisterVerticles(loadedVerticles);
+        listOfRegisteredVerticles.addAll(loadedVerticles);
+    }
+
+    /**
+     * Lifecycle method, override to change
+     * @param registeredVerticles
+     */
+    public void onRegisterVerticles(List<Class<? extends VertaceVerticle>> registeredVerticles) {
+
     }
 
 }
---
+++ src/main/java/io/vertace/core/LifecycleType.java
@@ -0,0 +1,7 @@
+package io.vertace.core;
+
+public enum LifecycleType {
+
+    LOAD_CLASS, REGISTER_VERTICLE, INITIALIZE
+
+}
