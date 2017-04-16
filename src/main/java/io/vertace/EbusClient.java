c05002ad6af8a5f79027689ee45e9612e10ea872
me <akter.hossain.ict@gmail.com>
2017-04-16 22:00:12 +0600

    refactoring

--- src/main/java/io/vertace/EbusClient.java
+++ src/main/java/io/vertace/EbusClient.java
@@ -1,8 +1,11 @@
 package io.vertace;
 
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.Message;
 
-public class EbusClient<R> {
+public class EbusClient {
 
     private final Vertx vertx;
     private final String address;
@@ -13,17 +16,21 @@ public class EbusClient<R> {
         this.address = address;
     }
 
-    public EbusClient of(String address) {
+    public static EbusClient of(String address) {
         return of(address, Vertx.vertx());
     }
 
-    public EbusClient of(String address, Vertx vertx) {
+    public static EbusClient of(String address, Vertx vertx) {
         return new EbusClient(vertx, address);
     }
 
-    public EbusClient send(Object msg) {
+    public EbusClient data(Object msg) {
         this.message = msg;
         return this;
+    }
+
+    public <R> void send(Handler<AsyncResult<Message<R>>> replyHandler) {
+        vertx.eventBus().send(address, message, replyHandler);
     }
 
 
